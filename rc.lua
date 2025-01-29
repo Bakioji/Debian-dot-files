f5cf0f9980435f1272db276835f9e4e51de0189a
Bakioji <kingoftsubaki@gmail.com>
2025-01-29 10:44:55 -0500

    Customization in lua

--- rc.lua
+++ rc.lua
@@ -123,9 +123,6 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Keyboard map indicator and switcher
 mykeyboardlayout = awful.widget.keyboardlayout()
 
--- {{{ Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
@@ -181,61 +178,31 @@ end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
+    -- Create the wibar for each screen
+    s.mywibar = awful.wibar({ position = "top", screen = s })
+    s.mywibar.height = 24
+    s.mywibar:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widget
+            layout = wibox.layout.fixed.horizontal,
+            -- Add widgets to your bar
+        },
+        nil, -- Middle widget (empty)
+        { -- Right widget
+            layout = wibox.layout.fixed.horizontal,
+            -- Add right widgets
+        },
+    }
+end)
+
+    -- Set wallpaper
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
-    }
 
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
-
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            mylauncher,
-            s.mytaglist,
-            s.mypromptbox,
-        },
-        s.mytasklist, -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
-            wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
-        },
-    }
-end)
--- }}}
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
@@ -541,9 +508,22 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 --Gaps
 beautiful.useless_gap  = 4
+-- Adjust the gap size as needed
+awful.layout.suit.tile.left.gaps = { top = 30 }
 
 --Autostart
 awful.spawn.with_shell("picom")
+awful.spawn.with_shell("polybar main")
 awful.spawn.with_shell("kitty")
 awful.spawn.with_shell("nitrogen --set-zoom-fill --random ~/Pictures/Wallpapers/") 
+
+-- Customize Naughty Notifications
+    -- position of notifications default = top_right
+naughty.config.defaults.position = "top_left"
+    -- add rounded corners
+naughty.config.defaults.shape = function(cr, width, height)
+    gears.shape.rounded_rect(cr, width, height, 10)
+end
+beautiful.notification_max_height = 200
+beautiful.notification_max_width = 350
 
